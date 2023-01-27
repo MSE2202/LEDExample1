edfdb7f37e5052ba33b2715953d23e6bb03667eb
naish-uwo <63020577+naish-uwo@users.noreply.github.com>
2023-01-27 14:43:27 -0500

    Generalize code and add comments

--- Example1.ino
+++ Example1.ino
@@ -5,6 +5,9 @@
   Author: Michael Naish
   Date: 23/01/26
 
+  This code will create a "sweep" of LEDs moving in sequence. For full effect, the number of LEDs in the sweep
+  pattern should be less than the total number of LEDs
+
   Rev 1 - Initial version 
   
 */
@@ -13,8 +16,8 @@ const int ledPins[] = {
   4, 16, 17, 18};                          // select the digital pins for the LEDs
 const int numberLEDs = 
   sizeof(ledPins) / sizeof(ledPins[0]);    // number of LEDs in display
+int sweep = 3;                             // number of LEDs in sweep pattern
 int state = 0;                             // current state of LEDs
-int sweep = 3;                             // number of LEDs in pattern
 unsigned long prevMillis = 0;              // start time for delay cycle, in milliseconds
 unsigned long curMillis = 0;               // current time, in milliseconds
 unsigned long rate = 300;                  // time delay for switching effect
@@ -27,18 +30,22 @@ void setup() {
 }
 
 void loop() {
-  curMillis = millis();                     // get the current time in milliseconds
-  if(curMillis - prevMillis > rate) {       // check to see if elapsed time matched the desired delay
-    prevMillis = curMillis;                 // update the start time for the next delay cycle
-    if(state < numberLEDs) {
-      digitalWrite(ledPins[state], HIGH);
+  curMillis = millis();                          // get the current time in milliseconds
+  if(curMillis - prevMillis > rate) {            // check to see if elapsed time matched the desired delay
+    prevMillis = curMillis;                      // update the start time for the next delay cycle
+    if(state <= numberLEDs && state > 0) {       // turn on LEDs for states 1 to numberLEDs 
+      digitalWrite(ledPins[state-1], HIGH);      // account for zero index of ledPins
     }
-    if(state >= sweep)
+    if(state >= sweep)                           // once number of LEDs in sweep has been turned on, start
+    {                                            // turning off trailing LEDs
+      digitalWrite(ledPins[state-sweep-1], LOW); // account for zero index of ledPins
+    }
+    if(state == 0)                               // clear last LED turned on from previous state sequence
     {
-      digitalWrite(ledPins[state-sweep], LOW);
+      digitalWrite(ledPins[numberLEDs-1], LOW);  // account for zero index of ledPins
     }
-    state++;
-    if(state == numberLEDs+sweep)
-       state = 0;
+    state++;                                     // shift to next state
+    if(state == numberLEDs+sweep)                // if past last state (with zero index)
+       state = 0;                                // reset state to zero to restart sequence
   }
 }
